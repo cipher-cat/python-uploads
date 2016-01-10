517f60a1ea6bc4a9a64f34debe333482d69a924f
Jellyfish <jellyfish@riseup.net>
2016-01-10 16:45:46 -0600

    test no3

---
+++ quickpythonscript.py
@@ -0,0 +1,17 @@
+#Python scipt "begins" in the next line.. :P
+print('Initializing Python Script..')
+completion=True
+print('Initialization completion',completion)
+name=input('Enter your name:')
+print('Ohai',name,'I am series of lines of code built with inteactivity in mind ^>^...')
+print('..today I shall demonstrate my capabilities..these are:'),print('Identifying valuaes:Int,float,str, and bool')
+print('You will now type in some data and I will identify it for you')
+num1=input('enter a whole number')
+num2=input('enter another whole number')
+print('input is:',type(num1),type(num2) )
+total=num1+num2
+print('total is',total, type(total))
+total=int(num1)+int(num2)
+print('total is',total,type(total))
+total=float(num1)+float(num2)
+print('total is',str(total),type(total))
